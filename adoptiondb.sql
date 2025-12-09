18cb04fdff7ad8f97d5c815c0123b5847fac33c5
hakdog456 <dustingualberto7@gmail.com>
2025-12-09 17:01:30 +0800

    awfaw

--- adoptiondb.sql
+++ adoptiondb.sql
@@ -1,72 +1,64 @@
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 09, 2025 at 10:00 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
 -- Database: `adoptiondb`
--- --------------------------------------------------------
-
-CREATE DATABASE IF NOT EXISTS `adoptiondb`;
-USE `adoptiondb`;
+--
 
 -- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
--- --------------------------------------------------------
+--
+
 CREATE TABLE `users` (
-  `userId` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) NOT NULL,
   `name` text NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `priveledge` varchar(50) NOT NULL,
-  PRIMARY KEY (`userId`),
-  UNIQUE KEY `username` (`username`)
+  `email` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Sample data
-INSERT INTO `users` (`userId`, `name`, `username`, `password`, `priveledge`) VALUES
-(1, 'Dustin Gualberto', 'hakdog', 'webprogdabest', 'admin'),
-(21, 'mennard Ezekiel', 'mennard', 'gwapoAko', 'user'),
-(23, 'Kyran Solomon', 'ky', 'heynow', 'user');
+--
+-- Indexes for dumped tables
+--
 
--- --------------------------------------------------------
--- Table structure for table `pets`
--- --------------------------------------------------------
-CREATE TABLE `pets` (
-  `petID` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `type` varchar(50) NOT NULL,
-  `breed` varchar(50) NOT NULL,
-  `age` int(11) NOT NULL,
-  `price` double NOT NULL,
-  `details` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`details`)),
-  PRIMARY KEY (`petID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userId`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
 
--- --------------------------------------------------------
--- Table structure for table `likedpet`
--- --------------------------------------------------------
-CREATE TABLE `likedpet` (
-  `likedPetId` int(11) NOT NULL AUTO_INCREMENT,
-  `userId` int(11) NOT NULL,
-  `petId` int(11) NOT NULL,
-  PRIMARY KEY (`likedPetId`),
-  UNIQUE KEY `user_pet_unique` (`userId`,`petId`),
-  CONSTRAINT `fk_likedpet_user` FOREIGN KEY (`userId`) REFERENCES `users`(`userId`) ON UPDATE CASCADE ON DELETE CASCADE,
-  CONSTRAINT `fk_likedpet_pet` FOREIGN KEY (`petId`) REFERENCES `pets`(`petID`) ON UPDATE CASCADE ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
--- --------------------------------------------------------
--- Table structure for table `transactions`
--- --------------------------------------------------------
-CREATE TABLE `transactions` (
-  `transactionId` int(11) NOT NULL AUTO_INCREMENT,
-  `petId` int(11) NOT NULL,
-  `userId` int(11) NOT NULL,
-  `userPayment` double NOT NULL,
-  `dateTimeCreated` datetime NOT NULL DEFAULT current_timestamp(),
-  `meetGreetDateTime` datetime NOT NULL,
-  `status` varchar(50) NOT NULL,
-  `location` text NOT NULL,
-  `evaluation` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`evaluation`)),
-  PRIMARY KEY (`transactionId`),
-  UNIQUE KEY `pet_user_unique` (`petId`,`userId`),
-  KEY `userId_to_transaction` (`userId`),
-  CONSTRAINT `fk_transaction_pet` FOREIGN KEY (`petId`) REFERENCES `pets`(`petID`) ON UPDATE CASCADE ON DELETE CASCADE,
-  CONSTRAINT `fk_transaction_user` FOREIGN KEY (`userId`) REFERENCES `users`(`userId`) ON UPDATE CASCADE ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
