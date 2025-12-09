b0f295e4a3c69480604bd822a5e05490c0b06e05
hakdog456 <dustingualberto7@gmail.com>
2025-12-09 22:46:40 +0800

    awdawd

--- adoptiondb.sql
+++ adoptiondb.sql
@@ -3,11 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
-<<<<<<< HEAD
--- Generation Time: Dec 09, 2025 at 01:22 PM
-=======
--- Generation Time: Dec 09, 2025 at 11:15 AM
->>>>>>> dbea35327524842574ff9b5fadbed0604bc58336
+-- Generation Time: Dec 09, 2025 at 03:46 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -50,7 +46,6 @@ CREATE TABLE `pets` (
   `breed` varchar(50) NOT NULL,
   `age` int(11) NOT NULL,
   `price` double NOT NULL,
-<<<<<<< HEAD
   `details` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`details`)),
   `imageDirectory` varchar(255) NOT NULL,
   `adoptedById` int(11) DEFAULT NULL
@@ -72,11 +67,6 @@ INSERT INTO `pets` (`petID`, `name`, `type`, `breed`, `age`, `price`, `details`,
 (14, 'Lunch', 'Dog', 'Bichon Frise', 1, 20000, '{\"gender\":\"Male\",\"color\":\"White\",\"coat_length\":\"Long\",\"personality\":\"Lunch is a playful and affectionate Bichon Frise who loves being the center of attention. He enjoys cuddles, playing fetch, and making new friends. His fluffy white coat requires regular grooming but his loving personality makes it all worthwhile.\",\"vaccinated\":true,\"spayed_neutered\":false}', 'images/homeImages/petPics/Lunch-BICHON_FRISE 1.webp', NULL),
 (15, 'Gizmo', 'Dog', 'Papillon', 1, 18000, '{\"gender\":\"Male\",\"color\":\"White and Brown\",\"coat_length\":\"Long\",\"personality\":\"Gizmo is a smart and alert Papillon with distinctive butterfly-like ears. He\'s highly trainable, loves learning new tricks, and is great with older children. His elegant appearance and lively personality make him a wonderful companion.\",\"vaccinated\":true,\"spayed_neutered\":false}', 'images/homeImages/petPics/Gizmo-PAPILLON 1.webp', NULL);
 
-=======
-  `details` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`details`))
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
->>>>>>> dbea35327524842574ff9b5fadbed0604bc58336
 -- --------------------------------------------------------
 
 --
@@ -94,6 +84,13 @@ CREATE TABLE `transactions` (
   `location` text NOT NULL,
   `evaluation` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`evaluation`))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `transactions`
+--
+
+INSERT INTO `transactions` (`transactionId`, `petId`, `userId`, `userPayment`, `dateTimeCreated`, `meetGreetDateTime`, `status`, `location`, `evaluation`) VALUES
+(1, 14, 27, 0, '2025-12-09 22:18:45', '2025-12-20 08:00:00', 'Application Approved', 'tarlac', '{\"applicantInfo\":{\"firstName\":\"dustin\",\"middleName\":\"briones\",\"lastName\":\"gualberto\",\"suffix\":\"\",\"occupation\":\"\",\"employer\":\"\",\"employerAddress\":\"\",\"email\":\"dustingualberto7@gmail.com\",\"phone\":\"09687488130\",\"address\":\"\"},\"homeEnvironment\":{\"housing\":\"House\",\"rent\":\"No\",\"landlordName\":\"\",\"landlordPhone\":\"\",\"adultsInHousehold\":\"\",\"childrenInHousehold\":\"\",\"otherPets\":\"No\",\"previousPets\":\"No\",\"averageAloneTime\":\"\"},\"petPreferences\":{\"reasons\":[\"Companion for child\"],\"otherReasonDetail\":\"\",\"gift\":\"No\",\"giftRecipientName\":\"\",\"giftRecipientPhone\":\"\",\"financialPrepared\":\"Yes\"},\"agreement\":{\"understand\":true,\"certify\":true,\"signature\":\"dustin\"},\"submittedAt\":\"2025-12-09T14:18:45.214Z\",\"placeholderPetId\":true,\"meetAndGreet\":{\"date\":\"2025-12-20 08:00pm:00\",\"location\":\"tarlac\",\"message\":\"awdadasd\"}}');
 
 -- --------------------------------------------------------
 
@@ -115,10 +112,11 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`userId`, `name`, `username`, `password`, `privilege`, `email`) VALUES
-(27, 'dustin', 'hakdog', '$2y$10$aN5zrbrFUktU9rkG/UIiPeqDWwZ.bS/wfLE2jxEvmK1.oPc2LEsw.', 'user', 'dustingualberto7@gmail.com'),
-(28, 'kyran', 'kyky', '$2y$10$nVjpza/ERG5XdLRmH9VFyOriHOZHnvV4wRQqdSfYZYbmhOJ1qXyx2', 'user', 'kyran@gmail.com'),
+(27, 'dustin', 'hakdog', '$2y$10$aN5zrbrFUktU9rkG/UIiPeqDWwZ.bS/wfLE2jxEvmK1.oPc2LEsw.', 'admin', 'dustingualberto7@gmail.com'),
+(28, 'kyran', 'kyky', '$2y$10$nVjpza/ERG5XdLRmH9VFyOriHOZHnvV4wRQqdSfYZYbmhOJ1qXyx2', 'admin', 'kyran@gmail.com'),
 (29, 'mennard', 'nardy', '$2y$10$qHZNKZqJmPW0LBk56MZ6vusxGfcfEn.SelxkkDBnZF4ry1tCWzhcK', 'user', 'nardy@gmail.com'),
-(32, 'chingchong', 'cheng', '$2y$10$daKG8dK6KjnMRZk.HLAAJeKz8ZheDIQRwmllGw1xyqWsoIyt01Pu2', 'user', 'cheng@gmail.com');
+(32, 'chingchong', 'cheng', '$2y$10$daKG8dK6KjnMRZk.HLAAJeKz8ZheDIQRwmllGw1xyqWsoIyt01Pu2', 'user', 'cheng@gmail.com'),
+(33, 'hope', 'lovejoyhope', '$2y$10$cB0qt3RNZDDYsY3niRE62eReXCdwOTjp.wiS/LO/rJkjbeuf3H6dO', 'user', 'hope@gmail.com');
 
 --
 -- Indexes for dumped tables
@@ -168,23 +166,19 @@ ALTER TABLE `likedpet`
 -- AUTO_INCREMENT for table `pets`
 --
 ALTER TABLE `pets`
-<<<<<<< HEAD
   MODIFY `petID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
-=======
-  MODIFY `petID` int(11) NOT NULL AUTO_INCREMENT;
->>>>>>> dbea35327524842574ff9b5fadbed0604bc58336
 
 --
 -- AUTO_INCREMENT for table `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `transactionId` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `transactionId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- Constraints for dumped tables
