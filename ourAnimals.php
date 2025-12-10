ef2daba94ede469d3592b7ca5af6d43c7b2c7754
hakdog456 <dustingualberto7@gmail.com>
2025-12-10 12:03:40 +0800

    awdawd

--- ourAnimals.php
+++ ourAnimals.php
@@ -37,6 +37,7 @@ if (isset($_GET['json'])) {
     <div id="petsData" data-pets='<?php echo htmlspecialchars(json_encode($pets, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), ENT_QUOTES, 'UTF-8'); ?>' hidden></div>
 
     <div class="nav">
+        <!-- Burger menu button for mobile -->
         <div class="burgerMenuCon" >
             <div class="burgerMenuBtn" >
                 <div></div>
@@ -51,20 +52,24 @@ if (isset($_GET['json'])) {
         <div class="mainNav">
             <p><a href="index.html">Home</a></p>
             <p> <a href="aboutUs.html">About Us</a></p>
-            <p> <a href="ourAnimals.php">Our Animals</a></p>
+            <p> <a href="ourAnimals.html">Our Animals</a></p>
             <p><a href="contact.html">Contact Us</a></p>
             <p><a href="faqs.html">FAQs</a></p>
         </div>
         <div class="navControls">
+            <!-- Search SVG -->
             <svg class="searchIcon" width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M18.3333 31.6667C25.6971 31.6667 31.6667 25.6971 31.6667 18.3333C31.6667 10.9695 25.6971 5 18.3333 5C10.9695 5 5 10.9695 5 18.3333C5 25.6971 10.9695 31.6667 18.3333 31.6667Z" stroke="#223125" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                 <path d="M35 35L27.75 27.75" stroke="#223125" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>
 
-            <svg class="userIcon" width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M33.3337 35V31.6667C33.3337 29.8986 32.6313 28.2029 31.381 26.9526C30.1308 25.7024 28.4351 25 26.667 25H13.3337C11.5655 25 9.86986 25.7024 8.61961 26.9526C7.36937 28.2029 6.66699 29.8986 6.66699 31.6667V35" stroke="#223125" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                <path d="M19.9997 18.3333C23.6816 18.3333 26.6663 15.3486 26.6663 11.6667C26.6663 7.98477 23.6816 5 19.9997 5C16.3178 5 13.333 7.98477 13.333 11.6667C13.333 15.3486 16.3178 18.3333 19.9997 18.3333Z" stroke="#223125" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            </svg>
+            <!-- User Svg -->
+            <a href="user-profile.html">
+                <svg class="userIcon" width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M33.3337 35V31.6667C33.3337 29.8986 32.6313 28.2029 31.381 26.9526C30.1308 25.7024 28.4351 25 26.667 25H13.3337C11.5655 25 9.86986 25.7024 8.61961 26.9526C7.36937 28.2029 6.66699 29.8986 6.66699 31.6667V35" stroke="#223125" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M19.9997 18.3333C23.6816 18.3333 26.6663 15.3486 26.6663 11.6667C26.6663 7.98477 23.6816 5 19.9997 5C16.3178 5 13.333 7.98477 13.333 11.6667C13.333 15.3486 16.3178 18.3333 19.9997 18.3333Z" stroke="#223125" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            </a>
 
         </div>
     </div>
